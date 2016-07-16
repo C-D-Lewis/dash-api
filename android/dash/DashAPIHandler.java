190ccd93d9bb90d385c30a202d7786f8e35500f8
Chris Lewis <Bonsitm@gmail.com>
2016-07-16 23:09:00 +0100

    Add Android files

---
+++ android/dash/DashAPIHandler.java
@@ -0,0 +1,69 @@
+package dash;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+public class DashAPIHandler {
+
+    public static void handleGetData(Context context, int type, PebbleDictionary out) {
+        switch(type) {
+            case DashAPIKeys.DataTypeBatteryPercent:
+                Intent batteryStatus = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+                int level = batteryStatus.getIntExtra(BatteryManager.EXTRA_LEVEL, -1);
+                int scale = batteryStatus.getIntExtra(BatteryManager.EXTRA_SCALE, -1);
+                out.addInt32(DashAPIKeys.AppKeyDataValue, Math.round((level / scale) * 100.0F));
+                break;
+            case DashAPIKeys.DataTypeGSMOperatorName:
+                break;
+            case DashAPIKeys.DataTypeGSMStrength:
+                break;
+            case DashAPIKeys.DataTypeWifiNetworkName:
+                break;
+            case DashAPIKeys.DataTypeStorageFreePercent:
+                break;
+            case DashAPIKeys.DataTypeStorageFreeGBMajor:
+                break;
+            case DashAPIKeys.DataTypeStorageFreeGBMinor:
+                break;
+        }
+    }
+
+    public static void handleSetFeature(Context context, int featureType, int featureState, PebbleDictionary out) {
+        switch(featureType) {
+            case DashAPIKeys.FeatureTypeWifi:
+                break;
+            case DashAPIKeys.FeatureTypeBluetooth:
+                break;
+            case DashAPIKeys.FeatureTypeRinger:
+                break;
+            case DashAPIKeys.FeatureTypeAutoSync:
+                break;
+            case DashAPIKeys.FeatureTypeHotSpot:
+                break;
+            case DashAPIKeys.FeatureTypeAutoBrightness:
+                break;
+        }
+    }
+
+    public static void handleGetFeature(Context context, int featureType, PebbleDictionary out) {
+        switch(featureType) {
+            case DashAPIKeys.FeatureTypeWifi:
+                break;
+            case DashAPIKeys.FeatureTypeBluetooth:
+                break;
+            case DashAPIKeys.FeatureTypeRinger:
+                break;
+            case DashAPIKeys.FeatureTypeAutoSync:
+                break;
+            case DashAPIKeys.FeatureTypeHotSpot:
+                break;
+            case DashAPIKeys.FeatureTypeAutoBrightness:
+                break;
+        }
+    }
+
+}
---
+++ android/dash/DashAPIService.java
@@ -0,0 +1,94 @@
+package dash;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.BatteryManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.getpebble.android.kit.PebbleKit;
+import com.getpebble.android.kit.util.PebbleDictionary;
+
+import java.util.UUID;
+
+public class DashAPIService extends Service {
+
+    private static final String TAG = DashAPIService.class.getName();
+    private static final boolean DEBUG = false;
+
+    private void parse(PebbleDictionary dict, UUID uuid) {
+        Context context = getApplicationContext();
+        PebbleDictionary out = new PebbleDictionary();
+
+        // Get data request
+        if(dict.getInteger(DashAPIKeys.RequestTypeGetData) != null) {
+            out.addInt32(DashAPIKeys.RequestTypeGetData, 0);
+
+            int type = dict.getInteger(DashAPIKeys.AppKeyDataType).intValue();
+            out.addInt32(DashAPIKeys.AppKeyDataType, type);
+            DashAPIHandler.handleGetData(context, type, out);
+        }
+
+        // Set feature request
+        if(dict.getInteger(DashAPIKeys.RequestTypeSetFeature) != null) {
+            out.addInt32(DashAPIKeys.RequestTypeSetFeature, 0);
+
+            int type = dict.getInteger(DashAPIKeys.AppKeyFeatureType).intValue();
+            int state = dict.getInteger(DashAPIKeys.AppKeyFeatureState).intValue();
+            DashAPIHandler.handleSetFeature(context, type, state, out);
+        }
+
+        // Get feature request
+        if(dict.getInteger(DashAPIKeys.RequestTypeGetFeature) != null) {
+            out.addInt32(DashAPIKeys.RequestTypeGetFeature, 0);
+
+            int type = dict.getInteger(DashAPIKeys.AppKeyFeatureType).intValue();
+            DashAPIHandler.handleGetFeature(context, type, out);
+        }
+
+        if(out.size() > 1) {    // At least the RequestType tuple
+            PebbleKit.sendDataToPebble(getApplicationContext(), uuid, out);
+            Log.d(TAG, "Sent response to " + uuid.toString());
+
+            if(DEBUG) {
+                Log.d(TAG, "JSON out: " + out.toJsonString());
+            }
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent == null) {
+            Log.e(TAG, "Intent was null");
+            return super.onStartCommand(intent, flags, startId);
+        }
+
+        try {
+            Log.d(TAG, "onStartCommand()");
+
+            //Get dictionary and parse
+            Bundle extras = intent.getExtras();
+            String jsonData = extras.getString("json");
+            if(DEBUG) {
+                Log.d(TAG, "JSON in: " + jsonData);
+            }
+
+            String uuid = extras.getString("uuid");
+            parse(PebbleDictionary.fromJson(jsonData), UUID.fromString(uuid));
+        } catch (Exception e) {
+            Log.e(TAG, "onStartCommand() threw exception: " + e.getLocalizedMessage());
+            e.printStackTrace();
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+}
