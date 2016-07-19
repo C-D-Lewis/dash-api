9663d474bc6aab0d900a75afe14520bfa301672c
Chris Lewis <Bonsitm@gmail.com>
2016-07-19 10:04:16 +0100

    Add handling of is_available response

--- src/c/pebble-dash-api.c
+++ src/c/pebble-dash-api.c
@@ -9,7 +9,8 @@
 typedef enum {
   RequestTypeGetData = 24784,
   RequestTypeSetFeature = 24785,
-  RequestTypeGetFeature = 24786
+  RequestTypeGetFeature = 24786,
+  RequestTypeIsAvailable = 24787
 } RequestType;
 
 typedef enum {
@@ -29,7 +30,7 @@ static DashAPIResultCallback *s_result_callback;
 
 static FeatureType s_last_get_feature_type, s_last_set_feature_type;
 static DataType s_last_get_data_type;
-static DictionaryIterator *s_iter;
+static DictionaryIterator *s_outbox;
 static char s_app_name[32];
 
 /********************************* Internal ***********************************/
@@ -50,7 +51,7 @@ static void clear_callbacks() {
 
 /**
  * Packet Formats 
- * (inbound):
+ * (From Android):
  *   RequestTypeGetData
  *     AppKeyDataType      - DataType
  *   RequestTypeSetFeature
@@ -58,35 +59,39 @@ static void clear_callbacks() {
  *     AppKeyFeatureState  - FeatureState
  *   RequestTypeGetFeature
  *     AppKeyFeatureType   - FeatureType
+ *   RequestTypeIsAvailable
+ *     AppKeyLibraryVersion
  *
- * (outbound):
+ * (To Android):
  *   RequestTypeGetData
  *     AppKeyDataType      - DataType
- *     AppKeyDataValue        - DataValue
+ *     AppKeyDataValue     - DataValue
  *   RequestTypeSetFeature
  *     AppKeyFeatureType   - FeatureType
  *     AppKeyFeatureState  - FeatureState
  *   RequestTypeGetFeature
  *     AppKeyFeatureType   - FeatureType
  *     AppKeyFeatureState  - FeatureState
+ *   RequestTypeIsAvailable
+ *     AppKeyResultCode    - ResultCodeNoPermissions | ResultCodeWrongVersion
  */
-static void inbox_received_handler(DictionaryIterator *s_iter, void *context) {
+static void inbox_received_handler(DictionaryIterator *inbox, void *context) {
   if(!in_flight()) {
     APP_LOG(APP_LOG_LEVEL_ERROR, "Dash API: no callbacks");
     return;  // Nothing expected, ignore
   }
 
   // Get data response
-  if(dict_find(s_iter, RequestTypeGetData)) {
+  if(dict_find(inbox, RequestTypeGetData)) {
     DataValue value;
 
-    int type = dict_find(s_iter, AppKeyDataType)->value->int32;
+    int type = dict_find(inbox, AppKeyDataType)->value->int32;
     switch(type) {
       // Data type will be integer
       case DataTypeBatteryPercent:
       case DataTypeGSMStrength:
       case DataTypeStoragePercentUsed:
-        value.integer_value = dict_find(s_iter, AppKeyDataValue)->value->int32;
+        value.integer_value = dict_find(inbox, AppKeyDataValue)->value->int32;
         s_last_get_data_cb(type, value);
         break;
 
@@ -95,7 +100,7 @@ static void inbox_received_handler(DictionaryIterator *s_iter, void *context) {
       case DataTypeGSMOperatorName:
       case DataTypeStorageFreeGBString:
         value.string_value = malloc(INBOX_SIZE);
-        strcpy(value.string_value, dict_find(s_iter, AppKeyDataValue)->value->cstring);
+        strcpy(value.string_value, dict_find(inbox, AppKeyDataValue)->value->cstring);
         s_last_get_data_cb(type, value);
         free(value.string_value);
         break;
@@ -103,18 +108,32 @@ static void inbox_received_handler(DictionaryIterator *s_iter, void *context) {
   }
 
   // Set feature response
-  else if(dict_find(s_iter, RequestTypeSetFeature)) {
-    int type = dict_find(s_iter, AppKeyFeatureType)->value->int32;
-    int state = dict_find(s_iter, AppKeyFeatureState)->value->int32;
+  else if(dict_find(inbox, RequestTypeSetFeature)) {
+    int type = dict_find(inbox, AppKeyFeatureType)->value->int32;
+    int state = dict_find(inbox, AppKeyFeatureState)->value->int32;
     s_last_set_feature_cb(type, state);
   }
 
   // Get feature response
-  else if(dict_find(s_iter, RequestTypeGetFeature)) {
-    int type = dict_find(s_iter, AppKeyFeatureType)->value->int32;
-    int state = dict_find(s_iter, AppKeyFeatureState)->value->int32;
+  else if(dict_find(inbox, RequestTypeGetFeature)) {
+    int type = dict_find(inbox, AppKeyFeatureType)->value->int32;
+    int state = dict_find(inbox, AppKeyFeatureState)->value->int32;
     s_last_get_feature_cb(type, state);
   } 
+
+  // Is available result, or no permission result
+  else if(dict_find(inbox, RequestTypeIsAvailable)) {
+    int code = dict_find(inbox, AppKeyResultCode)->value->int32;
+    switch(code) {
+      case ResultCodeNoPermissions:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "Permission for this app has not been granted within the Dash API Android app!");
+        break;
+      case ResultCodeWrongVersion:
+        APP_LOG(APP_LOG_LEVEL_ERROR, "An incompatible version of the Dash API Android app is installed!");
+        break;
+    }
+    s_result_callback(code);
+  }
 
   else {
     APP_LOG(APP_LOG_LEVEL_ERROR, "Dash API: Unknown message type");
@@ -125,8 +144,8 @@ static void inbox_received_handler(DictionaryIterator *s_iter, void *context) {
 
 static void write_header() {
   const int dummy = 0;
-  dict_write_int(s_iter, AppKeyUsesDashAPI, &dummy, sizeof(int), true);
-  dict_write_cstring(s_iter, AppKeyAppName, s_app_name);
+  dict_write_int(s_outbox, AppKeyUsesDashAPI, &dummy, sizeof(int), true);
+  dict_write_cstring(s_outbox, AppKeyAppName, s_app_name);
 }
 
 static bool prepare_outbox() {
@@ -142,7 +161,7 @@ static bool prepare_outbox() {
     return false;
   }
 
-  AppMessageResult result = app_message_outbox_begin(&s_iter);
+  AppMessageResult result = app_message_outbox_begin(&s_outbox);
   if(result != APP_MSG_OK) {
     APP_LOG(APP_LOG_LEVEL_ERROR, "Dash API: Error opening outbox!");
     s_result_callback(ResultCodeSendingFailed);
@@ -173,8 +192,8 @@ void dash_api_get_data(DataType type, DashAPIDataCallback *callback) {
   }
 
   const int dummy = 0;
-  dict_write_int(s_iter, RequestTypeGetData, &dummy, sizeof(int), true);
-  dict_write_int(s_iter, AppKeyDataType, &type, sizeof(int), true);
+  dict_write_int(s_outbox, RequestTypeGetData, &dummy, sizeof(int), true);
+  dict_write_int(s_outbox, AppKeyDataType, &type, sizeof(int), true);
 
   s_last_get_data_type = type;
   s_last_get_data_cb = callback;
@@ -187,10 +206,10 @@ void dash_api_set_feature(FeatureType type, FeatureState new_state, DashAPIFeatu
   }
 
   const int dummy = 0;
-  dict_write_int(s_iter, RequestTypeSetFeature, &dummy, sizeof(int), true);
-  dict_write_int(s_iter, AppKeyFeatureType, &type, sizeof(int), true);
+  dict_write_int(s_outbox, RequestTypeSetFeature, &dummy, sizeof(int), true);
+  dict_write_int(s_outbox, AppKeyFeatureType, &type, sizeof(int), true);
   const int state = (int)new_state; // Prevents 2 becoming 119762434
-  dict_write_int(s_iter, AppKeyFeatureState, &state, sizeof(int), true);
+  dict_write_int(s_outbox, AppKeyFeatureState, &state, sizeof(int), true);
 
   s_last_set_feature_type = type;
   s_last_set_feature_cb = callback;
@@ -203,8 +222,8 @@ void dash_api_get_feature(FeatureType type, DashAPIFeatureCallback *callback) {
   }
 
   const int dummy = 0;
-  dict_write_int(s_iter, RequestTypeGetFeature, &dummy, sizeof(int), true);
-  dict_write_int(s_iter, AppKeyFeatureType, &type, sizeof(int), true);
+  dict_write_int(s_outbox, RequestTypeGetFeature, &dummy, sizeof(int), true);
+  dict_write_int(s_outbox, AppKeyFeatureType, &type, sizeof(int), true);
 
   s_last_get_feature_type = type;
   s_last_get_feature_cb = callback;
@@ -226,7 +245,7 @@ void dash_api_is_available() {
   }
 
   const int version = DASH_API_VERSION;
-  dict_write_int(s_iter, AppKeyLibraryVersion, &version, sizeof(int), true);
+  dict_write_int(s_outbox, AppKeyLibraryVersion, &version, sizeof(int), true);
 
   send_outbox();
 }
